eb76f20e15cce8367a4a14eb04dcfbacb9de3e80
gengbushuang <gengbushuang@gengbushuang-L1.corp.apus.com>
2018-05-15 16:42:20 +0800

    dnf

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,8 @@
+
+public class TestMain {
+	public static void main(String[] args) {
+		double pow = Math.pow((1-4), 2);
+		System.out.println(1/(1+Math.sqrt(pow)));
+		System.out.println(Math.tanh(3.0));
+	}
+}
